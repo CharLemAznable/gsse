22cfc34a7c73221fec567f709eb1391a72ab497c
CharLemAznable <545541819@qq.com>
2024-03-10 01:17:29 +0800

    Fixed unit test.

--- gsse_z_unit_test.go
+++ gsse_z_unit_test.go
@@ -1,17 +1,12 @@
 package gsse_test
 
 import (
-	"bufio"
-	"bytes"
 	"fmt"
 	"github.com/CharLemAznable/gsse"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gctx"
 	"github.com/gogf/gf/v2/test/gtest"
-	"github.com/gogf/gf/v2/text/gstr"
 	"github.com/gogf/gf/v2/util/guid"
-	"io"
-	"net/http"
 	"testing"
 	"time"
 )
@@ -65,46 +60,15 @@ func Test_SendMessageWithId(t *testing.T) {
 	})
 }
 
-type eventSource struct {
-	Lines chan string
-	Done  chan error
-}
-
-func newEventSource(url string) *eventSource {
-	es := &eventSource{
-		Lines: make(chan string),
-		Done:  make(chan error),
-	}
-
-	go func() {
-		resp, err := http.Get(url)
-		if err != nil {
-			es.Done <- err
-			return
-		}
-		defer func() { _ = resp.Body.Close() }()
-
-		scanner := bufio.NewScanner(resp.Body)
-		for scanner.Scan() {
-			es.Lines <- scanner.Text()
-		}
-
-		if err := scanner.Err(); err != nil {
-			es.Done <- err
-		} else {
-			es.Done <- io.EOF
-		}
-	}()
-
-	return es
-}
-
 func Test_SendEvent(t *testing.T) {
-	clientCh := make(chan *gsse.Client, 1)
 	s := g.Server(guid.S())
 	s.BindHandler("/sse", gsse.Handle(func(client *gsse.Client) {
 		client.KeepAlive()
-		clientCh <- client
+		go func(client *gsse.Client) {
+			<-time.After(time.Second)
+			client.SendEvent("test", "send event")
+			client.Close()
+		}(client)
 	}))
 	s.SetDumpRouterMap(false)
 	_ = s.Start()
@@ -113,39 +77,23 @@ func Test_SendEvent(t *testing.T) {
 	time.Sleep(100 * time.Millisecond)
 	gtest.C(t, func(t *gtest.T) {
 		prefix := fmt.Sprintf("http://127.0.0.1:%d", s.GetListenedPort())
-		es := newEventSource(prefix + "/sse")
+		client := g.Client()
+		client.SetPrefix(prefix)
 
-		go func() {
-			sseClient := <-clientCh
-			sseClient.SendEvent("test", "send event")
-			sseClient.Close()
-		}()
-		finish := make(chan interface{}, 1)
-		go func() {
-			var buffer bytes.Buffer
-			for {
-				select {
-				case line := <-es.Lines:
-					buffer.WriteString(line)
-					//buffer.WriteString("\n")
-				case <-es.Done:
-					t.Assert(gstr.TrimStr(buffer.String(), ":"),
-						"event:testdata:send event")
-					finish <- ""
-					return
-				}
-			}
-		}()
-		<-finish
+		t.Assert(client.GetContent(gctx.New(), "/sse"),
+			":\n\nevent:test\ndata:send event\n\n")
 	})
 }
 
 func Test_SendEventWithId(t *testing.T) {
-	clientCh := make(chan *gsse.Client, 1)
 	s := g.Server(guid.S())
 	s.BindHandler("/sse", gsse.Handle(func(client *gsse.Client) {
 		client.KeepAlive()
-		clientCh <- client
+		go func(client *gsse.Client) {
+			<-time.After(time.Second)
+			client.SendEventWithId("test", "send event", "2")
+			client.Close()
+		}(client)
 	}))
 	s.SetDumpRouterMap(false)
 	_ = s.Start()
@@ -154,30 +102,10 @@ func Test_SendEventWithId(t *testing.T) {
 	time.Sleep(100 * time.Millisecond)
 	gtest.C(t, func(t *gtest.T) {
 		prefix := fmt.Sprintf("http://127.0.0.1:%d", s.GetListenedPort())
-		es := newEventSource(prefix + "/sse")
+		client := g.Client()
+		client.SetPrefix(prefix)
 
-		go func() {
-			sseClient := <-clientCh
-			sseClient.SendEventWithId("test", "send event", "2")
-			time.Sleep(7 * time.Second)
-			sseClient.Close()
-		}()
-		finish := make(chan interface{}, 1)
-		go func() {
-			var buffer bytes.Buffer
-			for {
-				select {
-				case line := <-es.Lines:
-					buffer.WriteString(line)
-					//buffer.WriteString("\n")
-				case <-es.Done:
-					t.Assert(gstr.TrimStr(buffer.String(), ":"),
-						"event:testdata:send eventid:2")
-					finish <- ""
-					return
-				}
-			}
-		}()
-		<-finish
+		t.Assert(client.GetContent(gctx.New(), "/sse"),
+			":\n\nevent:test\ndata:send event\nid:2\n\n")
 	})
 }
